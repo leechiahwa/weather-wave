025405bd659a41b122ab5db62a1bf5937f362ead
leechiahwa <leechiahwa99@hotmail.com>
2024-11-13 11:30:35 +0800

    fix: fix CORS issue

--- Weather Wave/src/api/api.ts
+++ Weather Wave/src/api/api.ts
@@ -6,7 +6,7 @@ type ForecastType = 'current' | 'forecast';
 // Base URL configuration
 const API_KEY = '19a8e6f825ee4134be433211240110';
 const createBaseURL = (type: ForecastType) => 
-  `http://api.weatherapi.com/v1/${type}.json?key=${API_KEY}`;
+  `/api/${type}.json?key=${API_KEY}`;
 
 // Export base URLs for different endpoints
 export const currentWeatherURL = createBaseURL('current');
@@ -14,8 +14,8 @@ export const forecastWeatherURL = createBaseURL('forecast');
 
 // Alternative approach using an object map
 const endpoints = {
-  current: `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`,
-  forecast: `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}`
+  current: `/api/current.json?key=${API_KEY}`,
+  forecast: `/api/forecast.json?key=${API_KEY}`
 } as const;
 
 export const fetchCurrentWeather = async (city: string) => {
@@ -25,6 +25,5 @@ export const fetchCurrentWeather = async (city: string) => {
 
 export const fetchForecastData = async (city: string) => {
   const response = await axios.get(`${endpoints.forecast}&q=${city}&days=3`);
-  console.log(response.data);
   return response.data;
 };
--- Weather Wave/vite.config.ts
+++ Weather Wave/vite.config.ts
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://api.weatherapi.com/v1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   base: '/weather-wave',
   resolve: {
     alias: {
