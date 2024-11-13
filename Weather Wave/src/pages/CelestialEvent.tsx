711d03cf29158770de4e082cd99dcd422e463a68
leechiahwa <leechiahwa99@hotmail.com>
2024-11-13 11:05:10 +0800

    refactor: remove unnecessary React imports from About, CelestialEvent, and Forecast pages

--- Weather Wave/src/pages/CelestialEvent.tsx
+++ Weather Wave/src/pages/CelestialEvent.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function CelestialEvent() {
   return (
     <div className="p-8">
--- Weather Wave/src/pages/Forecast.tsx
+++ Weather Wave/src/pages/Forecast.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ForecastWeather from "../components/Forecast/ForecastWeather";
 
 function Forecast() {
